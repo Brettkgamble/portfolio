ef47b07d9255475eac0b5fd2eb133f187e48dd28
Brett Gamble <brettkgamble@gmail.com>
2023-01-04 17:04:38 -0700

    0014- Contact Form - Working

--- portfolio/app/contact/page.jsx
+++ portfolio/app/contact/page.jsx
@@ -6,45 +6,52 @@ import Input from '../../components/input';
 import TextArea from "../../components/text";
 
 export default function Page() {
-    const [values, setValues] = useState({
-            name: '',
-            email: '',
-            message: ''
-        }
-    )
-    const [errors, setErrors ] = useState()
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-    const handleSubmit = async(e) => {
-        e.preventDefault()
-        const errors = validate(values)
-        const isError = Object.keys(errors).length
-        if (isError && isError > 0) {
-            setErrors(errors)
-            return
-        }
-        try {
-            const res = await fetch('/api/send', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(values),
-            })
-            if(!res.ok){
-                setValues({name: '', message: '', email: ''})
-            }
-        } catch(error)  {
-            console.error(error)
-        }
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    const data = {
+      name,
+      email,
+      message,
+    };
+    fetch('/api/send', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).then((response)=> response.json())
+        .catch((error) => {
+            console.error('Error', error)
+        });
+
+  };
 
     return (
         <main className="flex flex-col justify-between items-center p-24  min-h-screen">
             <div className="px-3 pt-10 w-full md:w-4/5 lg:w-3/5 xl:w-2/5">
-                <form className="flex flex-col items-center w-full mx-auto">
-                    <Input  id="name" name="name" placeholder="Your Name" label="Your Name"/>
-                    <Input  id="email" name="email" placeholder="email@email.com" label="Your Email"/>
-                    <TextArea id="message" name="message" placeholder="Your message" label="Message"/>
+                <form className="flex flex-col items-center w-full mx-auto" onSubmit={handleSubmit}>
+                    <Input
+                        id="name"
+                        name="name"
+                        placeholder="Your Name"
+                        label="Your Name"
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <Input
+                        id="email"
+                        name="email"
+                        placeholder="email@email.com"
+                        label="Your Email"
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <TextArea
+                        id="message"
+                        name="message"
+                        placeholder="Your message"
+                        label="Message"
+                        onChange={e => setMessage(e.target.value)}
+                    />
 
                     <button className="w-full py-2 mt-6 text-lg text-white bg-purple-500 rounded-md
                                 outline-none active:bg-purple-600 focus:ring-2 focus:ring-purple-400
---
+++ portfolio/pages/api/send.js
@@ -0,0 +1,37 @@
+// https://github.com/sendgrid/sendgrid-nodejs/blob/main/docs/use-cases/single-email-single-recipient.md
+// NOTE: This only works from the server so you need to deploy to test due to CORS
+const mail = require('@sendgrid/mail')
+
+mail.setApiKey(process.env.SENDGRID_API_KEY)
+
+export default async (req, res) => {
+  const body = JSON.parse(req.body);
+
+  const message = `
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Message: ${body.message}
+  `;
+
+  const data = {
+    to: 'brettkgamble@gmail.com',
+    from: `brettkgamble@gmail.com`,
+    subject: `New message from ${body.name}`,
+    text: message,
+    html: message.replace(/\r\n/g, '<br />'),
+  };
+
+  await mail.send(data)
+      .then(() => {
+        res.status(200).json({ status: 'OK' });
+      })
+      .catch(error => {
+        console.error(error)
+        if (error.response) {
+          const {message, code, response} = error;
+        }
+      })
+
+
+};
+
