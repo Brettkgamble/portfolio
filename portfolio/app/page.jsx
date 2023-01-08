707759cefbf0614a5c5ea9c0f2e0d68ec4f5a1d8
Brett Gamble <brettkgamble@gmail.com>
2023-01-08 14:32:47 -0700

    0018 - Refactor styling on app/page.jsx to use tailwind

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -13,95 +13,95 @@ export default function Home() {
             </p>
           </div>
 
-      <div className={styles.grid}>
-        <h2 className={inter.className}>
-            My stack is <span>-&gt;</span>
-        </h2>
-      </div>
-      <div className={styles.grid}>
-        <div className={styles.center}>
-          <Image
-            // className={styles.logo}
-            src="/react.svg"
-            alt="React Logo"
-            width={120}
-            height={37}
-            priority
-          />
-        </div>
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={120}
-            height={37}
-            priority
-          />
-        </div>
-         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            width={120}
-            height={37}
-            priority
-          />
-        </div>
-        <div className={styles.center}>
-          <Image
-            // className={styles.logo}
-            src="/sanity-logo.png"
-            alt="Sanity.io Logo"
-            width={120}
-            height={37}
-            priority
-          />
-        </div>
-      </div>
+      {/*<div className={styles.grid}>*/}
+      {/*  <h2 className={inter.className}>*/}
+      {/*      My stack is <span>-&gt;</span>*/}
+      {/*  </h2>*/}
+      {/*</div>*/}
+      {/*<div className={styles.grid}>*/}
+      {/*  <div className={styles.center}>*/}
+      {/*    <Image*/}
+      {/*      // className={styles.logo}*/}
+      {/*      src="/react.svg"*/}
+      {/*      alt="React Logo"*/}
+      {/*      width={120}*/}
+      {/*      height={37}*/}
+      {/*      priority*/}
+      {/*    />*/}
+      {/*  </div>*/}
+      {/*  <div className={styles.center}>*/}
+      {/*    <Image*/}
+      {/*      className={styles.logo}*/}
+      {/*      src="/next.svg"*/}
+      {/*      alt="Next.js Logo"*/}
+      {/*      width={120}*/}
+      {/*      height={37}*/}
+      {/*      priority*/}
+      {/*    />*/}
+      {/*  </div>*/}
+      {/*   <div className={styles.center}>*/}
+      {/*    <Image*/}
+      {/*      className={styles.logo}*/}
+      {/*      src="/vercel.svg"*/}
+      {/*      alt="Vercel Logo"*/}
+      {/*      width={120}*/}
+      {/*      height={37}*/}
+      {/*      priority*/}
+      {/*    />*/}
+      {/*  </div>*/}
+      {/*  <div className={styles.center}>*/}
+      {/*    <Image*/}
+      {/*      // className={styles.logo}*/}
+      {/*      src="/sanity-logo.png"*/}
+      {/*      alt="Sanity.io Logo"*/}
+      {/*      width={120}*/}
+      {/*      height={37}*/}
+      {/*      priority*/}
+      {/*    />*/}
+      {/*  </div>*/}
+      {/*</div>*/}
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Resume <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            I have experienced many professional opportunities and you can read about them here.
-          </p>
-        </a>
+      {/*<div className={styles.grid}>*/}
+      {/*  <a*/}
+      {/*    href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"*/}
+      {/*    className={styles.card}*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    <h2 className={inter.className}>*/}
+      {/*      Resume <span>-&gt;</span>*/}
+      {/*    </h2>*/}
+      {/*    <p className={inter.className}>*/}
+      {/*      I have experienced many professional opportunities and you can read about them here.*/}
+      {/*    </p>*/}
+      {/*  </a>*/}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            About <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>A little less formal than my resume.</p>
-        </a>
+      {/*  <a*/}
+      {/*    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"*/}
+      {/*    className={styles.card}*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    <h2 className={inter.className}>*/}
+      {/*      About <span>-&gt;</span>*/}
+      {/*    </h2>*/}
+      {/*    <p className={inter.className}>A little less formal than my resume.</p>*/}
+      {/*  </a>*/}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Sites <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            See what I have built and what I am building.
-          </p>
-        </a>
-      </div>
+      {/*  <a*/}
+      {/*    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"*/}
+      {/*    className={styles.card}*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    <h2 className={inter.className}>*/}
+      {/*      Sites <span>-&gt;</span>*/}
+      {/*    </h2>*/}
+      {/*    <p className={inter.className}>*/}
+      {/*      See what I have built and what I am building.*/}
+      {/*    </p>*/}
+      {/*  </a>*/}
+      {/*</div>*/}
     </main>
   )
 }
