633cbe690017210937800ba53316d23222e73d94
Brett Gamble <brettkgamble@gmail.com>
2023-01-08 17:22:16 -0700

    0025 - from scratch

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -4,7 +4,24 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     return (
-        <main className="flex flex-col justify-between items-center p-6 h-screen bg-black">
-        </main>
+        <div className="h-screen bg-black">
+            <div className="flex flex-col items-start lg:items-center p-6">
+                  <div className="lg:border lg:rounded-xl lg:border-callout-border-rgb lg:px-4 lg:py-4">
+                    <p className="font-mono">
+                      Website and Application Development by Brett Gamble&nbsp;
+                    </p>
+                  </div>
+            </div>
+            <div className="flex justify-left p-6">
+                <div className="grow w-32"></div>
+                <div className="flex-initial w-96 text-white">
+                    <h2 className={inter.className}>
+                        My stack is <span>-&gt;</span>
+                    </h2>
+                </div>
+                <div className="grow w-32"></div>
+            </div>
+
+        </div>
     )
 }
