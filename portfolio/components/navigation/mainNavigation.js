923678655ecb045af855fa7f1e0cc35cbdec3ae1
Brett Gamble <brettkgamble@gmail.com>
2023-01-14 16:41:53 -0700

    0041 Common Header

--- portfolio/components/navigation/mainNavigation.js
+++ portfolio/components/navigation/mainNavigation.js
@@ -10,12 +10,12 @@ export default function MainNavigation(props) {
 
     return (
         <>
-           <nav className="z-50 w-full bg-nav-rgba items-end justify-between pt-2 pb-4 border-b border-blue-100">
+           <nav className="z-50 w-full bg-nav-rgba items-end justify-between pt-2 pb-4 border-b border-blue-100 ">
                {/*Small screen*/}
                <div className="flex sm:hidden">
                    <div className="flex flex-col items-center max-w-1100 lg:items-start p-6">
                        <div className="lg:border lg:rounded-xl lg:border-callout-border-rgb lg:px-4 lg:py-4">
-                           <p className="font-mono">
+                           <p className="font-mono text-white">
                                Website and Application Development by Brett Gamble&nbsp;
                            </p>
                        </div>
@@ -73,7 +73,7 @@ export default function MainNavigation(props) {
                        <nav className="flex relative h-14 w-full items-center pl-2 pr-2">
                            <div className="flex flex-col items-center max-w-1100 lg:items-start p-6">
                                <div className="lg:border lg:rounded-xl lg:border-callout-border-rgb lg:px-4 lg:py-4">
-                                   <p className="font-mono">
+                                   <p className="font-mono text-white">
                                        Website and Application Development by Brett Gamble&nbsp;
                                    </p>
                                </div>
