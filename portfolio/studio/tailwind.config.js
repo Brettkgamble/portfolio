0589f3562668c8bd4107dc10bf0b560659781f2a
Brett Gamble <brettkgamble@gmail.com>
2023-01-30 22:00:48 -0700

    0053 - Sanity V3 successfully migrated

---
+++ portfolio/studio/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/**/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./portfolio/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    maxWidth:{
+      '1100': '1100px',
+    },
+    extend: {
+      colors: {
+        'callout-rgb': 'rgb(20, 20, 20)',
+        'callout-border-rgb': 'rgba(108, 108, 108,.3)',
+        'card-rgb': 'rgb(100, 100, 100, 0.15)',
+        'card-border': 'rgb(200, 200, 200, 0.15)',
+        'loc-select': '#007ca7'
+      },
+      zIndex: {
+        '2': '2',
+      },
+      gridTemplateColumns:{
+        'page-4': 'repeat(4, minmax(25%, auto))'
+      }
+
+    },
+  },
+  plugins: [
+
+  ],
+}
