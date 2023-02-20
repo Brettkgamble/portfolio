a39d2af5d25d04f07fe4a2ca138b734c35371fe8
Brett Gamble <brettkgamble@gmail.com>
2023-02-20 13:49:07 -0700

    0079 - BlockContent

--- portfolio/app/(site)/about/page.js
+++ portfolio/app/(site)/about/page.js
@@ -44,7 +44,7 @@ export default function Resume() {
         <>
             <section className="relative grid bg-white  h-screen w-screen auto-rows-auto grid-flow-row md:grid-rows-1 md:grid-flow-col">
                 <div className="relative h-1/3 md:h-2/4 lg:h-2/3">
-                    <div className="relative pt-48 text-5xl font-roboto-400 text-blue-500 z-40 uppercase text-center tracking-wider">{person.name}</div>
+                    <div className="relative uppercase text-center font-roboto-400 text-blue-500 z-40 tracking-wider pt-16 text-2xl md:pt-32 md:text-3xl lg:pt-48 lg:text-5xl">{person.name}</div>
                     <div className="relative pt-8 text-base font-roboto-400 text-blue-500 z-40 uppercase text-center tracking-wider">A work in progress</div>
                     <Image
                         // className="profile mx-auto rounded-full py-2 w-16 "
@@ -63,27 +63,31 @@ export default function Resume() {
                 </div>
 
             </section>
-            <section className="relative grid grid-cols-12 pb-32 bg-white w-screen auto-rows-auto grid-flow-row -mt-60 justify-center md:grid-rows-1 md:grid-flow-col">
-                <div className="col-start-4 col-span-6 text-3xl font-roboto-600 font-bold text-black w-full uppercase text-start tracking-widest">ABOUT ME
-                   <hr className="mt-2 w-14 h-2 dark:bg-blue-700"/>
+            <section className="relative grid grid-cols-12 -mt-96 pb-32 bg-white w-screen auto-rows-auto grid-flow-row
+                        justify-center -mt-96  md:-mt-64 ">
+                <div className="pl-8  col-span-12 font-roboto-600 font-bold text-black w-full uppercase
+                        text-start tracking-widest  sm:-mt-32 md:col-span-8 md:col-start-4 md:-mt-24 lg:text-3xl lg:-mt-2 lg:pt-8 ">ABOUT ME
+                   <hr className="mt-2 w-14 h-2 dark:bg-blue-700 lg:w-24"/>
                 </div>
-                <div className="col-start-4 col-span-2 pt-16 bg-white flex flex-col items-center  w-48 h-full">
-                    <Image
-                        className="rounded-full "
-                        src={
-                            urlFor(person.image.asset.url)
-                            .height(600)
-                            .width(600)
-                            .url()
-                        }
-                        alt={person.image.alt}
-                        quality='100'
-                        width="600"
-                        height="600"
-                        priority
-                    />
+                <div className="col-span-12 ">
+                    <div className="flex bg-white flex-col justify-center h-full py-8">
+                        <Image
+                            className="mx-auto w-2/6 rounded-full"
+                            src={
+                                urlFor(person.image.asset.url)
+                                .height(600)
+                                .width(600)
+                                .url()
+                            }
+                            alt={person.image.alt}
+                            quality='100'
+                            width="600"
+                            height="600"
+                            priority
+                        />
+                    </div>
                 </div>
-                <div className="font-roboto-400 text-black col-start-6 col-span-4 pt-16 ">
+                <div className="px-8 font-roboto-400 text-black col-span-12 md:col-start-6 md:col-span-4 md:pt-16 ">
                     <div>
                         <BlockContent
                             style={{fontSize: "3rem"}}
@@ -95,10 +99,9 @@ export default function Resume() {
                         />
                     </div>
                 </div>
-                <div className="font-roboto-400 text-black col-start-4 col-span-6 pt-16 ">
-                    <div className="col-start-4 col-span-6 text-3xl font-roboto-600 font-bold text-black w-full uppercase text-start tracking-widest">Experience
-                        <hr className="mt-2 w-14 h-2 dark:bg-blue-700"/>
-                    </div>
+                <div className="pl-8   col-span-12 font-roboto-600 font-bold text-black w-full uppercase
+                        text-start tracking-widest  sm:-mt-32 md:col-span-8 md:col-start-4 md:-mt-24 lg:text-3xl lg:-mt-2 lg:pt-8 ">Experience
+                   <hr className="mt-2 w-14 h-2 dark:bg-blue-700 lg:w-24"/>
                 </div>
             </section>
             </>
