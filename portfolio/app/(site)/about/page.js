b4ccba4f35921271924ab0c8feed141ee12bec9e
Brett Gamble <brettkgamble@gmail.com>
2023-02-26 09:09:02 -0700

    00080 - Tailwind Flex for About

--- portfolio/app/(site)/about/page.js
+++ portfolio/app/(site)/about/page.js
@@ -41,38 +41,17 @@ export default function Resume() {
 
     const person = p[0]
     return (
-        <>
-            <section className="relative grid bg-white  h-screen w-screen auto-rows-auto grid-flow-row md:grid-rows-1 md:grid-flow-col">
-                <div className="relative h-1/3 md:h-2/4 lg:h-2/3">
-                    <div className="relative uppercase text-center font-roboto-400 text-blue-500 z-40 tracking-wider pt-16 text-2xl md:pt-32 md:text-3xl lg:pt-48 lg:text-5xl">{person.name}</div>
-                    <div className="relative pt-8 text-base font-roboto-400 text-blue-500 z-40 uppercase text-center tracking-wider">A work in progress</div>
-                    <Image
-                        // className="profile mx-auto rounded-full py-2 w-16 "
-                        className="profile mx-auto w-auto h-auto "
-                        src={person.splashImage.asset.url}
-                        alt={person.splashImage.alt}
-                        quality='100'
-                        // width="1000"
-                        // height="600"
-                        priority
-                        fill
-                        // sizes="(max-width: 768px) 100vw,
-                        //   (max-width: 1200px) 50vw,
-                        //   33vw"
-                    />
-                </div>
-
-            </section>
-            <section className="relative grid grid-cols-12 -mt-96 pb-32 bg-white w-screen auto-rows-auto grid-flow-row
-                        justify-center -mt-96  md:-mt-64 ">
-                <div className="pl-8  col-span-12 font-roboto-600 font-bold text-black w-full uppercase
-                        text-start tracking-widest  sm:-mt-32 md:col-span-8 md:col-start-4 md:-mt-24 lg:text-3xl lg:-mt-2 lg:pt-8 ">ABOUT ME
-                   <hr className="mt-2 w-14 h-2 dark:bg-blue-700 lg:w-24"/>
-                </div>
-                <div className="col-span-12 ">
-                    <div className="flex bg-white flex-col justify-center h-full py-8">
+        <div className="bg-white">
+            <div className="flex flex-wrap ">
+                <div className="hidden flex-none p-2 sm:flex sm:w-1/12 md:w-1/6"></div>
+                <div className="w-full flex-none pt-2 md:w-1/5 lg:w-1/6">
+                    <div className="pl-8 font-roboto-600 font-bold text-black w-full uppercase
+                        text-start tracking-widest  ">ABOUT ME
+                       <hr className="mt-2 w-14 h-2 dark:bg-blue-700"/>
+                    </div>
+                    <div className="flex bg-white flex-col justify-center py-8 ">
                         <Image
-                            className="mx-auto w-2/6 rounded-full"
+                            className="mx-auto w-3/6 rounded-full sm:w-2/5 md:w-3/4 lg:w-2/4"
                             src={
                                 urlFor(person.image.asset.url)
                                 .height(600)
@@ -87,23 +66,59 @@ export default function Resume() {
                         />
                     </div>
                 </div>
-                <div className="px-8 font-roboto-400 text-black col-span-12 md:col-start-6 md:col-span-4 md:pt-16 ">
-                    <div>
-                        <BlockContent
-                            style={{fontSize: "3rem"}}
-                            blocks={person.introduction[0].bio}
-                            serializers={serializers}
-                            projectId={client.projectId}
-                            dataset={client.dataset}
-                            imageOptions={{w: 500, h: 440, fit: 'max'}}
-                        />
+                <div className="w-full flex-none px-8 md:w-2/4 lg:w-2/4">
+                    {/*<div className="text-white text-center bg-blue-600 px-4 py-2 m-2 rounded-lg">2</div>*/}
+                    <div className=" font-roboto-400 text-black md:pt-16">
+                        <div>
+                            <BlockContent
+                                style={{fontSize: "3rem"}}
+                                blocks={person.introduction[0].bio}
+                                serializers={serializers}
+                                projectId={client.projectId}
+                                dataset={client.dataset}
+                                imageOptions={{w: 500, h: 440, fit: 'max'}}
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className="pl-8   col-span-12 font-roboto-600 font-bold text-black w-full uppercase
-                        text-start tracking-widest  sm:-mt-32 md:col-span-8 md:col-start-4 md:-mt-24 lg:text-3xl lg:-mt-2 lg:pt-8 ">Experience
-                   <hr className="mt-2 w-14 h-2 dark:bg-blue-700 lg:w-24"/>
+                <div className="hidden flex-none p-2 sm:flex sm:w-1/12 md:w-1/6"></div>
+
+                <div className="w-full flex-none p-2 md:w-2/5">
+                    <div className="text-white text-center bg-blue-600 px-4 py-2 m-2 rounded-lg">3</div>
                 </div>
-            </section>
-            </>
+                <div className="w-full flex-none p-2 md:w-2/5">
+                    <div className="text-white text-center bg-blue-600 px-4 py-2 m-2 rounded-lg">4</div>
+                </div>
+            </div>
+
+             {/*Grid Example*/}
+                {/*<div className="min-h-screen flex items-start justify-center">*/}
+                {/*    <div className="grid grid-cols-2 z-40">*/}
+                {/*        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">1</div>*/}
+                {/*        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">2</div>*/}
+                {/*        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">3</div>*/}
+                {/*        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">4</div>*/}
+                {/*        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">5</div>*/}
+                {/*        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">6</div>*/}
+                {/*        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">7</div>*/}
+                {/*        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">8</div>*/}
+                {/*        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">9</div>*/}
+                {/*        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">10</div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+                {/*<div className="grid grid-cols-2 z-40 md:grid-cols-3 lg:grid-cols-2 gap-4 p-5">*/}
+                {/*    <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">1</div>*/}
+                {/*    <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">2</div>*/}
+                {/*    <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">3</div>*/}
+                {/*    <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">4</div>*/}
+                {/*    <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">5</div>*/}
+                {/*    <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">6</div>*/}
+                {/*    <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">7</div>*/}
+                {/*    <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">8</div>*/}
+                {/*    <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">9</div>*/}
+                {/*    <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">10</div>*/}
+                {/*</div>*/}
+
+            </div>
     )
 }
