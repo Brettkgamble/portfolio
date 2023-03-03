aa9f90ae14af8e8463d2494a4dbfdcc12a213210
Brett Gamble <brettkgamble@gmail.com>
2023-03-03 16:52:32 -0700

    00092 - Layout

--- portfolio/app/(site)/about/page.js
+++ portfolio/app/(site)/about/page.js
@@ -50,28 +50,29 @@ export default function Resume() {
 
     const person = p[0]
     console.log('Person', person)
+
     return (
         <div className="bg-white pb-32">
-            <div className="flex flex-wrap ">
+            <div className="flex flex-wrap">
                 <div className="hidden flex-none p-2 sm:flex sm:w-1/12 md:w-1/6"></div>
-                <div className="w-full flex-none pt-2 md:w-1/5 lg:w-1/6">
-                    <div className="pl-8 font-roboto-600 font-bold text-black w-full uppercase
+                <div className=" flex-none pt-2 pl-8 w-11/12 md:w-1/5 lg:w-1/6">
+                    <div className=" font-roboto-600 font-bold text-black w-full uppercase
                         text-start tracking-widest  ">ABOUT ME
                        <hr className="mt-2 w-14 h-2 dark:bg-blue-700"/>
                     </div>
-                    <div className="flex bg-white flex-col justify-center py-8 ">
+                    <div className="relative flex bg-white flex-col justify-center py-8 sm:w-4/5 ">
                         <Image
-                            className="mx-auto w-3/6 rounded-full sm:w-2/5 md:w-3/4 lg:w-2/4"
+                            className="mx-auto w-1/3 rounded-full sm:w-2/5 md:w-3/4 lg:w-2/4"
                             src={
                                 urlFor(person.image.asset.url)
-                                .height(600)
-                                .width(600)
+                                .height(300)
+                                .width(300)
                                 .url()
                             }
                             alt={person.image.alt}
                             quality='100'
-                            width="600"
-                            height="600"
+                            width="120"
+                            height="120"
                             priority
                             placeholder="blur"
                             blurDataURL={person.image.asset.url}
@@ -108,19 +109,20 @@ export default function Resume() {
                 </div>
                 <div className="hidden flex-none p-2 sm:flex sm:w-1/12 md:w-1/6"></div>
                 <div className="hidden flex-none p-2 sm:flex sm:w-1/12 md:w-1/6"></div>
-                <div className="w-full flex-none pt-2 md:w-1/5 lg:w-1/6">
-                    <div className="pl-8 font-roboto-600 font-bold text-black w-full uppercase
+                <div className="w-full flex-none pl-8 pt-2 md:w-1/5 lg:w-1/6">
+                    <div className=" font-roboto-600 font-bold text-black w-full uppercase
                         text-start tracking-widest  ">Experience
                        <hr className="mt-2 w-14 h-2 dark:bg-blue-700"/>
                     </div>
                 </div>
-                <div className="w-full">
+                <div className="w-11/12 pl-8">
                         {person.organizations.map((org, id) => {
                             return (
-                                <div key ={id} className="flex pb-8">
+                                <div key ={id} className="flex flex-col w-fit pb-8">
+                                    {/*replace next div with 80% width??? that way we can remove all the css in it*/}
                                     <div className="hidden flex-none p-2 sm:flex sm:basis-1/6 md:basis-1/6"></div>
-                                    <div className="flex w-full  bg-white flex-col justify-start pt-4 text-black font-bold hover:text-blue-700
-                                    sm:basis-1/3 md:basis-1/6 lg:basis-1/6" >
+                                    <div className="flex basis-full  bg-white flex-col justify-start pt-4 text-black font-bold hover:text-blue-700
+                                        sm:basis-1/3 md:basis-1/3 lg:basis-1/3" >
                                         {/*sm:w-2/5 md:w-3/4 lg:w-2/4"*/}
                                         {/*md:w-1/5 lg:w-1/6*/}
                                         <span key={id}>
@@ -131,45 +133,55 @@ export default function Resume() {
                                                 {org.name}
                                             </Link>
                                         </span>
-                                        <div className="flex bg-white flex-col py-2 ">
+                                        <div className="relative overflow-hidden flex mx-auto object-center basis-full bg-white flex-col py-2 md:w-4/5">
                                             <Image
-                                                className=" w-full object-left-top "
+                                                className="flex pt-4 md:object-left-top "
                                                 src={
                                                     urlFor(org.image.asset.url)
-                                                    .height(400)
-                                                    .width(800)
+                                                    .height(350)
+                                                    .width(650)
+                                                    .crop("center")
                                                     .url()
                                                 }
+                                                style={{
+                                                    objectFit: "scale-down"
+                                                }}
                                                 alt={org.image.alt}
                                                 quality='100'
-                                                width="600"
-                                                height="600"
+
+                                                height="200"
+                                                width="200"
                                                 // priority
                                                 placeholder="blur"
                                                 blurDataURL={org.image.asset.url}
                                             />
                                         </div>
                                     </div>
-                                    <div className="w-full flex pl-16 pr-8 pt-4 justify-start  sm:basis-2/6 md:basis-2/3 lg:basis-5/6">
-                                        {/* md:w-2/4 lg:w-2/4*/}
-                                        {/*<div className="text-white text-center bg-blue-600 px-4 py-2 m-2 rounded-lg">2</div>*/}
-                                        <div className=" font-roboto-400 text-black ">
-                                            <div>
-                                                <div className="text-black font-bold pb-4 -ml-4">
-                                                    {person.organizations[0].roles[0].period}
+                                    {org.roles.map((role, roleid) => {
+                                        return (
+                                            <div key = {roleid}
+                                                 className="basis-full flex ml-8 pr-8 pt-4 justify-start
+                                                 sm:basis-2/6 md:basis-2/3 lg:basis-5/6">
+                                                <div className=" font-roboto-400 text-black ">
+                                                    <div>
+                                                        <div className="text-black font-bold pb-4 -ml-4">
+                                                            {role.period}
+                                                        </div>
+                                                        <BlockContent
+                                                            style={{fontSize: "3rem"}}
+                                                            blocks={role.role}
+                                                            serializers={serializers}
+                                                            projectId={client.projectId}
+                                                            dataset={client.dataset}
+                                                            imageOptions={{w: 500, h: 440, fit: 'max'}}
+                                                        />
+                                                    </div>
                                                 </div>
-                                                <BlockContent
-                                                    style={{fontSize: "3rem"}}
-                                                    blocks={person.organizations[0].roles[0].role}
-                                                    serializers={serializers}
-                                                    projectId={client.projectId}
-                                                    dataset={client.dataset}
-                                                    imageOptions={{w: 500, h: 440, fit: 'max'}}
-                                                />
                                             </div>
-                                        </div>
-                                    </div>
-                                    {/*<div className="hidden flex-none p-2 sm:flex sm:basis-1/6 md:basis-1/6"></div>*/}
+                                        )
+                                    })}
+
+                                    <div className="hidden flex-none p-2 sm:flex sm:basis-1/6 md:basis-1/6"></div>
                                 </div>
 
 
