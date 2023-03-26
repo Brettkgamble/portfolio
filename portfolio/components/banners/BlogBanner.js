dec9a64be083e615d03214e196559ddc41215f5c
Brett Gamble <brettkgamble@gmail.com>
2023-03-26 15:54:25 -0600

    00125 - BlogBanner.js replace apostrophe with &apos;

--- portfolio/components/banners/BlogBanner.js
+++ portfolio/components/banners/BlogBanner.js
@@ -2,7 +2,7 @@ function BlogBanner() {
     return (
         <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
             <div>
-                <h1>Brett Gamble's Blog</h1>
+                <h1>Brett Gamble&apos;s Blog</h1>
                 <h2 className="mt-5 md:mt-0">
                     Welcome to {" "}
                     <span className="underline decoration-4 decoration-[#F7AB0A]">
