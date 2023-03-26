ec3e66620b8643748277b49ba0629112cc97d8ee
Brett Gamble <brettkgamble@gmail.com>
2023-03-26 17:13:12 -0600

    00128 - At 1:43 in vid

--- portfolio/components/blog/BlogList.js
+++ portfolio/components/blog/BlogList.js
@@ -33,7 +33,7 @@ function BlogList({ posts }) {
                                 </div>
                                 <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
                                     {post.categories.map((category) => (
-                                        <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                                        <div key={category._id} className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
                                             <p>{category.title}</p>
                                         </div>
                                     ))}
