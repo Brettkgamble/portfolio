ab717a6e336562320af0d5f469533aebac67d99b
Brett Gamble <brettkgamble@gmail.com>
2023-03-30 06:56:49 -0600

    00136 - Working on categories

--- portfolio/app/(site)/blog/page.js
+++ portfolio/app/(site)/blog/page.js
@@ -38,10 +38,24 @@ export default async function Blog() {
     const posts = await client.fetch(query)
     console.log(posts)
     return (
-        <div className="max-w-7xl mx-auto">
-            <BlogHeader />
-            <BlogBanner />
-            <BlogList posts={posts}/>
+        <div>
+            <div className="max-w-7xl mx-auto">
+                <BlogHeader />
+                <BlogBanner />
+            </div>
+             <div className="max-w-7xl mx-auto">
+                 <hr className="border-[#F7AB0A] mb-10" />
+                 <div className="flex flex-wrap">
+                     <div className="w-full flex-none pt-16 px-8 md:w-2/4 lg:w-3/4">
+                        <BlogList posts={posts}/>
+                     </div>
+                     <div className=" flex-none text-white pt-16 pl-2 w-11/12 md:w-1/5 lg:w-1/5">
+                         <div className="grid grid-cols-1 md:grid-cols-3 gap-10 gap-y-16 pb-24">
+                             Categories
+                         </div>
+                     </div>
+                 </div>
+             </div>
         </div>
     )
 }
--- portfolio/components/blog/BlogList.js
+++ portfolio/components/blog/BlogList.js
@@ -7,7 +7,7 @@ import ClientSideRoute from "../ClientSideRoute";
 function BlogList({ posts }) {
     return (
         <div>
-            <hr className="border-[#F7AB0A] mb-10" />
+
             <div className="grid grid-cols-1 md:grid-cols-3 px-10 gap-10 gap-y-16 pb-24">
                 {/* Posts */}
                 {posts.map((post) => (
