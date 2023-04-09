cce3e2d014a27bb00ff816c6d158da296bf54b20
Brett Gamble <brettkgamble@gmail.com>
2023-04-09 08:53:42 -0600

    00159 - Added api/sitemap.js and added a re-write rule to next.config.js

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -11,6 +11,14 @@ const nextConfig = {
     config.experiments = { ...config.experiments, topLevelAwait: true }
     return config
   },
+    async rewrites() {
+      return [
+          {
+              source: '/sitemap.xml',
+              destination: '/api/sitemap'
+          }
+      ]
+    }
 }
 
 module.exports = nextConfig
